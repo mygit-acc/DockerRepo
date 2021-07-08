cfc22a5990a6ffc31e702d1084ce45dba8746677
mygit-acc <84027075+mygit-acc@users.noreply.github.com>
2021-07-08 11:43:00 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,16 @@
 provider "aws" {
-  version = "~> 3.3.0"
   region = "ap-south-1"
   shared_credentials_file = "${var.credentialfile}"
-  profile = "devops"
 }
 
 terraform {
   backend "s3" {
     bucket         = "kiranstatefilebucket"
-    encrypt        = true
-    key = "terraform.tfstate"
-    region = "ap-south-1"
-    profile = "devops"
+    encrypt        =  true
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    profile        = "devops"
  }
-}
-
-data "terraform_remote_state" "network" {
-  backend = "s3"
-  config = {
-    bucket         = "kiranstatefilebucket"
-    encrypt        = true
-    key = "terraform.tfstate"
-    region = "ap-south-1"
-  }
 }
 
 
